d424f911337edd4f98a39a248c7b5a26ae66dd05
Anton Olieinykov <anton.olieinykow.ext@siemens-energy.com>
2025-07-04 19:07:23 +0300

    update payments

--- src/routes/admin/payments/handlers.ts
+++ src/routes/admin/payments/handlers.ts
@@ -1,13 +1,25 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { db } from "../../../db/index.js";
-import { payments } from "../../../db/schema/payment.js";
+import {payments, paymentStatusEnum} from "../../../db/schema/payment.js";
+import {eq} from "drizzle-orm";
+import {profiles} from "../../../db/schema/index.js";
+import {integer, serial, timestamp} from "drizzle-orm/pg-core/index";
 
 export const getPayments = async (
     request: FastifyRequest,
     reply: FastifyReply
 ) => {
   try {
-    const data = await db.select().from(payments);
+    const data = await db.select({
+      id: payments.id,
+      profile: profiles,
+      amount: payments.amount,
+      status: payments.status,
+      createdAt: payments.createdAt,
+      updatedAt: payments.updatedAt,
+    })
+        .from(payments)
+        .leftJoin(profiles, eq(payments.profileId, profiles.id))
     return reply.code(200).send({
       success: true,
       data
